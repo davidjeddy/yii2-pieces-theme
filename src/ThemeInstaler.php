e93199907217c76598385ab3498ff3a87f21334d
David E <me@davidjeddy.com>
2015-10-19 00:23:02 -0400

    Iterated on ThemeInstaller postPackageEvent exec logic.

--- src/ThemeInstaler.php
+++ src/ThemeInstaler.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace davidjeddy;
+namespace davidjeddy\pieces;
 
 use Composer\Script\Event;
 use Composer\Installer\PackageEvent;
@@ -10,7 +10,11 @@ class ThemeInstaller
     public static function postPackageInstall(Event $event)
     {
         $composer = $event->getComposer();
-        echo 'asdf';
-        exec('ln -sfn frontent/web/theme/pieces vendor/davidjeddy/yii2-pieces-theme/src/');
+        echo('echo Attempting to creating link from theme package to Yii2 advanced frontend theme directory...')
+        exec('mkdir frontend/web/themes/');
+        exec('ln -sfn ../../../vendor/davidjeddy/yii2-pieces-theme/src/ frontend/web/themes/pieces');
+        exec('echo ...attempt completed.');
+
+        return true;
     }
 }
