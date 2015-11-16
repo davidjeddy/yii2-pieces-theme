23b3257ce18465b94f16b0554639db2f5095ad45
David E <me@davidjeddy.com>
2015-11-15 21:58:58 -0500

    ADDED Google Analytics script to theme

--- src/layouts/base.php
+++ src/layouts/base.php
@@ -60,4 +60,15 @@ $this->beginContent('@frontend/views/layouts/_clear.php');
     </div>
 </footer>
 
+<!-- Google Analytics script -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-70123684-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 <?php $this->endContent() ?>
