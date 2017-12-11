e8595922b70cb75e62013f71b2d968b3c6331f25
David J Eddy <me@davidjeddy.com>
2017-12-10 20:23:07 -0500

    ADDED logic that only pagination parent_category_id 1 (comic); AND will only pagination w/i its own category_id.

--- src/views/article/view.php
+++ src/views/article/view.php
@@ -27,18 +27,38 @@ use dosamigos\disqus\Comments;
         <?php endif; ?>
     </article>
 
-    <?php
-    if ($model->category_id === 1) {
-        $prev = $model->getNextOrPrev($model->category_id, $model->published_at, 'DESC', 'published_at');
-        $next = $model->getNextOrPrev($model->category_id, $model->published_at, 'ASC',  'published_at');
-        ?>
+    <div class="row">
+        <?php
+        // get parent category_id
+        $parent_cat_id = \davidjeddy\articlecategory\models\ArticleCategory::find()
+            ->andWhere(['id' => $model->category_id])
+            ->andWhere(['parent_id' => 1])
+            ->one();
 
-        <div class="row">
-            <div class="col-xs-3 text-left"><?php if (!empty($prev)) { ?><h2><a href="<?php echo ($prev['slug'] ? $prev['slug'] : NULL); ?>" class="prevnext">Prev.</a></h2><?php } ?></div>
-            <div class="col-xs-6 text-center"></div>
-            <div class="col-xs-3 text-right"><?php if (!empty($next)) { ?><h2><a href="<?php echo ($next['slug'] ? $next['slug'] : NULL); ?>" class="prevnext">Next</a></h2><?php } ?></div>
-        </div>
-    <?php }; ?>
+        // does the current category have children AND
+        // is the current categoery a child of 'comic'? If yes pagination per category_id, else skip paginateion
+        if ($parent_cat_id) {
+            $prev = $model->getNextOrPrev($model->category_id, $model->published_at, 'DESC', 'published_at');
+            $next = $model->getNextOrPrev($model->category_id, $model->published_at, 'ASC', 'published_at');
+
+            // prev. exists
+            if (!empty($prev)) {
+                echo '<div class="col-xs-3 text-left"><h2><a href="' . ($prev['slug'] ? $prev['slug'] : NULL) . '" class="prevnext">Prev.</a></h2></div>';
+            };
+            // one or the other do not
+            if (!empty($prev) && empty($next) || empty($prev) && !empty($next)) {
+                echo '<div class="col-xs-9 text-center"></div>';
+            } elseif (!empty($prev) && !empty($next)) {
+                // both exits
+                echo '<div class="col-xs-6 text-center"></div>';
+            }
+            // next exists
+            if (!empty($next)) {
+                echo '<div class="col-xs-3 text-right"><h2><a href="' . ($next['slug'] ? $next['slug'] : NULL) . '" class="prevnext">Next</a></h2></div>';
+            };
+        };
+        ?>
+    </div>
 
     <?php echo Comments::widget([
         // see http://help.disqus.com/customer/portal/articles/472098-javascript-configuration-variables
