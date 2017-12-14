5dfe21dc594ca4b4de92649bbba46bad42e5ee7c
David J Eddy <me@davidjeddy.com>
2017-12-13 21:55:04 -0500

    REMOVED pagination for site/index. Landing page should not paginate.

--- src/views/site/index.php
+++ src/views/site/index.php
@@ -30,36 +30,6 @@ $model = \common\models\Article::findOne(['id' => 45]);
         </article>
 
         <?php
-        /**
-         * Only show prev/next for 'comic's
-         * @source https://github.com/davidjeddy/pieces-compendium/issues/2
-         * @todo Base this off the category title, not id? - DJE - 2015-11-29
-         */
-        if ($model->category_id === 1 ) {
-            $prev = $model->getNextOrPrev($model->category_id, $model->published_at, 'DESC', 'published_at');
-            $next = $model->getNextOrPrev($model->category_id, $model->published_at, 'ASC',  'published_at');
-            ?>
-
-            <div class="row">
-                <div class="col-xs-3 text-left">
-                    <?php if (!empty($prev)) { ?>
-                    <h2>
-                        <a href="../article/<?php echo ($prev['slug'] ? $prev['slug'] : NULL); ?>" class="prevnext">Prev.</a>
-                    </h2>
-                    <?php } ?>
-                </div>
-                <div class="col-xs-6 text-center"></div>
-                <div class="col-xs-3 text-right">
-                    <?php if (!empty($next)) { ?>
-                    <h2>
-                        <a href="../article/<?php echo ($next['slug'] ? $next['slug'] : NULL); ?>" class="prevnext">Next</a>
-                    </h2>
-                    <?php } ?>
-                </div>
-            </div>
-        <?php }; ?>
-
-        <?php
         try {
             echo Comments::widget([
                 // see http://help.disqus.com/customer/portal/articles/472098-javascript-configuration-variables
