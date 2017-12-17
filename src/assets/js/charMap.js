640e122d1fd857772a630c395942a4d5bc6b2a5d
David J Eddy <me@davidjeddy.com>
2017-12-16 19:07:00 -0500

    UPGRADE to http media URLS.

--- src/assets/js/charMap.js
+++ src/assets/js/charMap.js
@@ -3,64 +3,64 @@
  */
 // create array with image items
 var imageArray = [
-    'http://storage.pieces-compendium.com/source/1/characterImageMap/0.png',
-    'http://storage.pieces-compendium.com/source/1/characterImageMap/1.png',
-    'http://storage.pieces-compendium.com/source/1/characterImageMap/2.png',
-    'http://storage.pieces-compendium.com/source/1/characterImageMap/3.png',
-    'http://storage.pieces-compendium.com/source/1/characterImageMap/4.png',
-    'http://storage.pieces-compendium.com/source/1/characterImageMap/5.png',
-    'http://storage.pieces-compendium.com/source/1/characterImageMap/6.png',
-    'http://storage.pieces-compendium.com/source/1/characterImageMap/7.png',
-    'http://storage.pieces-compendium.com/source/1/characterImageMap/8.png',
-    'http://storage.pieces-compendium.com/source/1/characterImageMap/9.png',
-    'http://storage.pieces-compendium.com/source/1/characterImageMap/10.png',
-    'http://storage.pieces-compendium.com/source/1/characterImageMap/11.png',
-    'http://storage.pieces-compendium.com/source/1/characterImageMap/12.png',
-    'http://storage.pieces-compendium.com/source/1/characterImageMap/13.png',
+    'https://storage.pieces-compendium.com/source/1/characterImageMap/0.png',
+    'https://storage.pieces-compendium.com/source/1/characterImageMap/1.png',
+    'https://storage.pieces-compendium.com/source/1/characterImageMap/2.png',
+    'https://storage.pieces-compendium.com/source/1/characterImageMap/3.png',
+    'https://storage.pieces-compendium.com/source/1/characterImageMap/4.png',
+    'https://storage.pieces-compendium.com/source/1/characterImageMap/5.png',
+    'https://storage.pieces-compendium.com/source/1/characterImageMap/6.png',
+    'https://storage.pieces-compendium.com/source/1/characterImageMap/7.png',
+    'https://storage.pieces-compendium.com/source/1/characterImageMap/8.png',
+    'https://storage.pieces-compendium.com/source/1/characterImageMap/9.png',
+    'https://storage.pieces-compendium.com/source/1/characterImageMap/10.png',
+    'https://storage.pieces-compendium.com/source/1/characterImageMap/11.png',
+    'https://storage.pieces-compendium.com/source/1/characterImageMap/12.png',
+    'https://storage.pieces-compendium.com/source/1/characterImageMap/13.png',
 ];
 
 // character page image map
 Image0 = new Image(800,800);
-Image0.src = 'http://storage.pieces-compendium.com/source/1/characterImageMap/0.png';
+Image0.src = 'https://storage.pieces-compendium.com/source/1/characterImageMap/0.png';
 
 Image1 = new Image(800,800);
-Image1.src = 'http://storage.pieces-compendium.com/source/1/characterImageMap/1.png';
+Image1.src = 'https://storage.pieces-compendium.com/source/1/characterImageMap/1.png';
 
 Image2 = new Image(800,800);
-Image2.src = 'http://storage.pieces-compendium.com/source/1/characterImageMap/2.png';
+Image2.src = 'https://storage.pieces-compendium.com/source/1/characterImageMap/2.png';
 
 Image3 = new Image(800,800);
-Image3.src = 'http://storage.pieces-compendium.com/source/1/characterImageMap/3.png';
+Image3.src = 'https://storage.pieces-compendium.com/source/1/characterImageMap/3.png';
 
 Image4 = new Image(800,800);
-Image4.src = 'http://storage.pieces-compendium.com/source/1/characterImageMap/4.png';
+Image4.src = 'https://storage.pieces-compendium.com/source/1/characterImageMap/4.png';
 
 Image5 = new Image(800,800);
-Image5.src = 'http://storage.pieces-compendium.com/source/1/characterImageMap/5.png';
+Image5.src = 'https://storage.pieces-compendium.com/source/1/characterImageMap/5.png';
 
 Image6 = new Image(800,800);
-Image6.src = 'http://storage.pieces-compendium.com/source/1/characterImageMap/6.png';
+Image6.src = 'https://storage.pieces-compendium.com/source/1/characterImageMap/6.png';
 
 Image7 = new Image(800,800);
-Image7.src = 'http://storage.pieces-compendium.com/source/1/characterImageMap/7.png';
+Image7.src = 'https://storage.pieces-compendium.com/source/1/characterImageMap/7.png';
 
 Image8 = new Image(800,800);
-Image8.src = 'http://storage.pieces-compendium.com/source/1/characterImageMap/8.png';
+Image8.src = 'https://storage.pieces-compendium.com/source/1/characterImageMap/8.png';
 
 Image9 = new Image(800,800);
-Image9.src = 'http://storage.pieces-compendium.com/source/1/characterImageMap/9.png';
+Image9.src = 'https://storage.pieces-compendium.com/source/1/characterImageMap/9.png';
 
 Image10 = new Image(800,800);
-Image10.src = 'http://storage.pieces-compendium.com/source/1/characterImageMap/10.png';
+Image10.src = 'https://storage.pieces-compendium.com/source/1/characterImageMap/10.png';
 
 Image11 = new Image(800,800);
-Image11.src = 'http://storage.pieces-compendium.com/source/1/characterImageMap/11.png';
+Image11.src = 'https://storage.pieces-compendium.com/source/1/characterImageMap/11.png';
 
 Image12 = new Image(800,800);
-Image12.src = 'http://storage.pieces-compendium.com/source/1/characterImageMap/12.png';
+Image12.src = 'https://storage.pieces-compendium.com/source/1/characterImageMap/12.png';
 
 Image13 = new Image(800,800);
-Image13.src = 'http://storage.pieces-compendium.com/source/1/characterImageMap/13.png';
+Image13.src = 'https://storage.pieces-compendium.com/source/1/characterImageMap/13.png';
 /**
  * Swap the DOM.character.src value
  */
--- src/assets/js/comicMap.js
+++ src/assets/js/comicMap.js
@@ -4,33 +4,33 @@
 // create array with image items
 var imageArray = [
     // comic page image map
-    'http://storage.pieces-compendium.com/source/1/comicImageMap/14.png',
-    'http://storage.pieces-compendium.com/source/1/comicImageMap/15.png',
-    'http://storage.pieces-compendium.com/source/1/comicImageMap/16.png',
-    'http://storage.pieces-compendium.com/source/1/comicImageMap/17.png',
-    'http://storage.pieces-compendium.com/source/1/comicImageMap/18.png',
-    'http://storage.pieces-compendium.com/source/1/comicImageMap/19.png'
+    'https://storage.pieces-compendium.com/source/1/comicImageMap/14.png',
+    'https://storage.pieces-compendium.com/source/1/comicImageMap/15.png',
+    'https://storage.pieces-compendium.com/source/1/comicImageMap/16.png',
+    'https://storage.pieces-compendium.com/source/1/comicImageMap/17.png',
+    'https://storage.pieces-compendium.com/source/1/comicImageMap/18.png',
+    'https://storage.pieces-compendium.com/source/1/comicImageMap/19.png'
 ];
 
 // comic page image map
 // TODO why does including these result in a fast DOM changes? dunno, but leave these here.
 Image0 = new Image(800,3150);
-Image0.src = 'http://storage.pieces-compendium.com/source/1/comicImageMap/14.png';
+Image0.src = 'https://storage.pieces-compendium.com/source/1/comicImageMap/14.png';
 
 Image1 = new Image(800,3150);
-Image1.src = 'http://storage.pieces-compendium.com/source/1/comicImageMap/15.png';
+Image1.src = 'https://storage.pieces-compendium.com/source/1/comicImageMap/15.png';
 
 Image2 = new Image(800,3150);
-Image2.src = 'http://storage.pieces-compendium.com/source/1/comicImageMap/16.png';
+Image2.src = 'https://storage.pieces-compendium.com/source/1/comicImageMap/16.png';
 
 Image3 = new Image(800,3150);
-Image3.src = 'http://storage.pieces-compendium.com/source/1/comicImageMap/17.png';
+Image3.src = 'https://storage.pieces-compendium.com/source/1/comicImageMap/17.png';
 
 Image4 = new Image(800,3150);
-Image4.src = 'http://storage.pieces-compendium.com/source/1/comicImageMap/18.png';
+Image4.src = 'https://storage.pieces-compendium.com/source/1/comicImageMap/18.png';
 
 Image5 = new Image(800,3150);
-Image5.src = 'http://storage.pieces-compendium.com/source/1/comicImageMap/19.png';
+Image5.src = 'https://storage.pieces-compendium.com/source/1/comicImageMap/19.png';
 
 /**
  * Swap the DOM.character.src value
